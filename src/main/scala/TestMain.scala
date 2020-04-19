c23c3263d1d2f33c2c91ff8e59d50418f6dbf44a
Stefan <stefan.vucic.research@gmail.com>
2020-04-20 01:06:04 +0200

    update TestMain for requested changes

--- src/main/scala/TestMain.scala
+++ src/main/scala/TestMain.scala
@@ -5,17 +5,12 @@ object TestMain {
   def main(args: Array[String]): Unit = {
     println("Hello world")
 
-    println("Next is print of new class")
+    println("\nNext is print of first object")
     val nc = new FirstClass(45)
     nc.printVal()
 
-
-    println("One more println")
-
-    println("One more println in a row for pull request")
-
     val n = FirstClass(721)
-    println(s"This is value for new instance:\n" + n.toString)
+    println(s"\nThis is value for second object:\n" + n.toString)
 
 
 
